cd8426af5d6de28594f26ebcce2a7b90cdc42599
Nonso M <nmogor@hamilton.edu>
2020-09-18 02:54:12 -0400

    Frontend update with Flask

--- Frontend/app.py
+++ Frontend/app.py
@@ -9,9 +9,23 @@ app = Flask(__name__)
 def home():
     return render_template("index.html") # display the html page
 
+@app.route("/advancedSearch.html")
+def displayAdvanced():
+    return render_template("advancedSearch.html") # display the html page
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"]) # From basic search
 def my_form_post():
+    # text = request.form["query"]
+    # processed_text = text.upper()
+
+    return render_template("resultsPage.html")
+
+@app.route("/resultsPage.html")
+def results():
+    return render_template("resultsPage.html")
+
+@app.route("/advancedSearch.html", methods=["POST", "GET"]) # From advanced search
+def my_form_post2():
     # text = request.form["query"]
     # processed_text = text.upper()
 
@@ -21,5 +35,6 @@ def my_form_post():
 # def displayResults():
 #     return render_template("resultsPage.html")
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, use_reloader = False)
