f938e0a6896545507e9b3f6e0733a0e46285e127
Nonso M <nmogor@hamilton.edu>
2020-09-25 04:15:53 -0400

    attached a small, sample database to flask, and it can be queried

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,39 +1,111 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from flask_mysqldb import MySQL 
 
 app = Flask(__name__)
 
-# def hello_world():
-#     return "Hello World2"
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "Shakers!"
+app.config["MYSQL_DB"] = "sample-db"
 
-@app.route("/")
-def home():
-    return render_template("index.html") # display the html page
+mysql = MySQL(app)
 
-@app.route("/advancedSearch.html")
-def displayAdvanced():
-    return render_template("advancedSearch.html") # display the html page
+# BASIC SEARCH 
 
-@app.route("/", methods=["POST", "GET"]) # From basic search
-def my_form_post():
-    # text = request.form["query"]
-    # processed_text = text.upper()
+# POST is to send/change data.
+@app.route("/", methods=["POST", "GET"])
+def basicSearch():
+    if(request.method == "GET"):
+        return render_template("index.html")
+    else: # POST
+        if(not request.form["query"]):
+            return render_template("index.html") # maybe display a flash message here 
 
-    return render_template("resultsPage.html")
+        query = request.form["query"] # name in brackets matches the name of the post form in the HTML
+        enteredText = query
+        queryString = f"SELECT articleid from article where author = '{query}';"
+        print(queryString)
+        curr = mysql.connection.cursor()
+        curr.execute(queryString)
+        fetchdata = curr.fetchall()
+        curr.close()
+        
+        # return render_template("resultsPage.html", query=query) # query(left) is the name of the variable we put in the html. 
+        return redirect(url_for("basicResults1", query=fetchdata, enteredText = enteredText)) # put in the function of the url you want to go to
 
-@app.route("/resultsPage.html")
-def results():
-    return render_template("resultsPage.html")
+
+
+# ADVANCED SEARCH
 
 @app.route("/advancedSearch.html", methods=["POST", "GET"]) # From advanced search
-def my_form_post2():
-    # text = request.form["query"]
-    # processed_text = text.upper()
+def displayAdvanced():
+    print("Start")
+    if(request.method == "GET"):
+        return render_template("advancedSearch.html")
+    else: # POST
+        if(not request.form["query"] and not request.form.getlist("checkbox")): # if no boxes checked and nothing entered
+            return render_template("advancedSearch.html")
+        elif(request.form.getlist("checkbox")): # If we have a box checked
+            # print("Boxes checked:", request.form.getlist("checkbox"))
+            topics = request.form.getlist("checkbox")
+        else: # if no checkboxes checked, and just text entered
+            topics = None
+            # print("topics:", topics)
+        query = request.form["query"] # name matches the name of the post form in the HTML
+        query = query.upper()
+        print("query:", query)
+            # return render_template("resultsPage.html", query=query) # query(left) is the name of the variable in results.html. Right var is the one here. 
+        if(query and topics):
+            return redirect(url_for("advancedResults", query=query, topics = topics)) # put in the function of the url you want to go to
+        elif(not topics):
+            print("why?")
+            return redirect(url_for("basicResults1", query = query)) # put in the function of the url you want to go to
+        elif(not query):
+            return redirect(url_for("basicResults2", topics = topics)) # put in the function of the url you want to go to
+        else:
+            print("Here: ",topics, query)
+            return render_template("advancedSearch.html")
 
-    return render_template("resultsPage.html")
+# RESULTS
 
-# @app.route("/results")
-# def displayResults():
-#     return render_template("resultsPage.html")
+# def results(query=None, topics=None, topicOrQuery=None): # this input str var will eventually get converted into a SQL query to be ran against the database to return results.
+#     # if(request.method == "GET"):
+#     if(topicOrQuery):
+#         if(topicOrQuery.isalnum()):
+#             query = topicOrQuery
+#         else:
+#             topics = topicOrQuery
+
+#     print(query)
+#     print("topics:", topics) # when we don't have a query the hyphen stays. So we'll have to strip it later on
+#     return render_template("resultsPage.html", query=query, topics=topics)
+@app.route("/resultsPage/<query>-<enteredText>", methods=["POST", "GET"]) # since we redirected it, it expects to see <query> and <topics> in the URL
+def basicResults1(enteredText, query=None): # this input str var will eventually get converted into a SQL query to be ran against the database to return results.
+    # this is to figure out if it's a topic or query
+    print(type(query))
+    query = query.split("),")
+    print(query)
+    return render_template("resultsPage.html", query=query, enteredText=enteredText)#, topics=topics)
+
+@app.route("/resultsPage/<topics>", methods=["POST", "GET"]) # since we redirected it, it expects to see <query> and <topics> in the URL
+def basicResults2(topics=None): # this input str var will eventually get converted into a SQL query to be ran against the database to return results.
+    # this is to figure out if it's a topic or query
+
+    return render_template("resultsPage.html", topics=topics)
+
+@app.route("/resultsPage/<query>-<topics>", methods=["POST", "GET"]) # since we redirected it, it expects to see <query> and <topics> in the URL
+def advancedResults(query=None, topics=None, topicOrQuery=None): # this input str var will eventually get converted into a SQL query to be ran against the database to return results.
+    # if(request.method == "GET"):
+    if(topicOrQuery):
+        if(topicOrQuery.isalnum()):
+            query = topicOrQuery
+        else:
+            topics = topicOrQuery
+
+    print(query)
+    print("topics:", topics) # when we don't have a query the hyphen stays. So we'll have to strip it later on
+    return render_template("resultsPage.html", query=query, topics=topics)
+
 
 
 if __name__ == "__main__":
