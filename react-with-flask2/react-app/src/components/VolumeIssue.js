b5e21d994e5730b4507cef18a951f5b204bf99b7
Nonso M <nmogor@hamilton.edu>
2020-10-25 18:09:08 -0400

    React frontend almost back where it used to be

---
+++ react-with-flask2/react-app/src/components/VolumeIssue.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import shakerLogo from '../images/shaker-manifesto-logo.PNG';
+import hamiltonLogo from '../images/hamilton-lib-logo.PNG';
+// import Form from './components/Form'
+
+function VolumeIssue() {
+  return (
+    
+      <p>Volume and Issue</p>
+         
+  );
+}
+
+export default VolumeIssue;
