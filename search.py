3deb83ceb9b8dd9309a130cbb81ecf4e8194fc6c
nwadair <35549523+nwadair@users.noreply.github.com>
2020-10-26 13:07:28 -0400

    initial version of fuzzy search (possible over match problem) only allows character replacement not character deletion

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 from suffix_trees import STree
+from SMTree import FuzzyTree
 import sys, re, os, json, pickle, time
 
 DIRECTORY_NAME=".\\Volume01\\"
@@ -24,7 +25,7 @@ def create_tree(dic_name, debug=False):
                             total_dict[total_index] = (v, i, a, index)
                             total_index += 1
                             index += 1
-        return (STree.STree(total), total_dict)
+        return (FuzzyTree(total.lower()), total_dict)
 
 # INPUT: list with elements (article id, index)
 # OUTPUT: list with elements (article id, index, occurances)
@@ -43,8 +44,8 @@ def search(inp, articleIDs=[]):
     assert(inp)
     global SUFFIX_TREE, SUFFIX_DICT
     if not SUFFIX_TREE:
-        SUFFIX_TREE, SUFFIX_DICT = create_tree(DIRECTORY_NAME, True)
-    if results := SUFFIX_TREE.find_all(inp.lower()):
+        SUFFIX_TREE, SUFFIX_DICT = create_tree(DIRECTORY_NAME)
+    if results := SUFFIX_TREE.find(inp.lower()):
         l = simplify_results([(int("{:02d}{:02d}{:03d}".format(*SUFFIX_DICT[i][:3])), SUFFIX_DICT[i][3]) for i in sorted(results)])
         previews = previewlist(DIRECTORY_NAME, [ ("{:07d}.txt".format(elem[0]), elem[1]) for elem in l], inp.lower())
         # previews = previewlist(DIRECTORY_NAME, [("{:02d}{:02d}{:03d}.txt".format(*SUFFIX_DICT[i][:3]), SUFFIX_DICT[i][3]) for i in sorted(results)], inp.lower())
@@ -67,7 +68,7 @@ def previewlist(dictname, locations, searchstring):
     results = []
     for filename, index in locations:
         with open(os.path.join(dictname, filename), "rb") as file:
-            results.append("...{}...\n".format(preview(file, index, searchstring)))
+            results.append("...{}...".format(preview(file, index, searchstring)))
     return results
 
 def main():
