7c0a739201ee8cb7672c7d2d90f2e3e5dfa82774
Nonso M <nmogor@hamilton.edu>
2020-10-30 19:14:53 -0400

    Changed path of txt file

--- react-with-flask2/flask-server/autocomplete.py
+++ react-with-flask2/flask-server/autocomplete.py
@@ -4,7 +4,7 @@ AUTOCOMPLETE = None
 
 def init():
     names = None
-    with open("names.txt") as file:
+    with open("../../shaker_dictionary.txt") as file:
         names = file.readlines()
 
     words = dict(zip(names, [dict()] * len(names)))
