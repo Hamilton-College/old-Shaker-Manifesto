ba592fa95b37085b830e88054318aee099650c01
nwadair <35549523+nwadair@users.noreply.github.com>
2020-11-14 13:19:24 -0500

    final version of ngram based search no major revisions to search should be necessary

--- ngram_search.py
+++ ngram_search.py
@@ -3,21 +3,18 @@ from suffix_trees import STree
 import sys, re, os, json, pickle, time
 
 DIRECTORY_NAME=os.path.join(".", "react-with-flask2", "flask-server", "Volume01")
-PAGE_LIMIT=15
+PAGE_LIMIT=15 #number of results per page
 
 class Result:
     def __init__(self, terms, thresh, raw_index, dict):
-        print(terms)
-        self.terms = terms
-        self.thresh = thresh
-        self.raw_index = raw_index
+        self.terms = terms          #list of search terms contained in result
+        self.thresh = thresh        #combined search threshold via ngram similarity
+        self.raw_index = raw_index  #raw index from entire search directory
         self.vol = -1
         self.issue = -1
         self.article = -1
-        self.index = 0
+        self.index = 0              #index within specified article
         self.preview = None
-        self._loc = DIRECTORY_NAME
-        self.occur = 1
         self._processRaw(dict)
 
     def getTerm(self):
@@ -38,29 +35,32 @@ class Result:
     def getArticle(self):
         return self.article
 
-    def getOccur(self):
-        return self.occur
-
     def _processRaw(self, dict):
+        """sets volume, issue, article, and index fields based on the passed
+        dictionary and raw_index"""
         self.vol, self.issue, self.article, self.index = dict[self.raw_index]
 
     def filename(self):
+        """only callable after _processRaw"""
         assert(self.vol != -1)
         assert(self.issue != -1)
         assert(self.article != -1)
-        return os.path.join(DIRECTORY_NAME,
-            "{:02d}{:02d}{:03d}.txt".format(self.vol, self.issue, self.article))
+        return os.path.join(DIRECTORY_NAME, "{:07d}.txt".format(self.id()))
 
     def id(self):
+        """only callable after _processRaw"""
         return self.vol * 100000 + self.issue * 1000 + self.article
 
     def getPreview(self):
+        """sets preview field if no preview is stored and returns the stored
+        preview in all cases"""
         if self.preview is None:
             self._set_preview()
         return self.preview
 
     def _set_preview(self):
-        start = self.index - 100 if self.index - 100 > 0 else 0
+        """sets the preview field and inserts bolding for frontend display"""
+        start = max([self.index - 100, 0])
         with open(self.filename(), "rb") as file:
             file.seek(start)
             self.preview = re.sub("({})".format("|".join(self.terms)),
@@ -70,19 +70,22 @@ class Result:
 
 class SM_Search:
     def __init__(self):
-        self._tree = None
-        self._index_dict = None
-        self._ngram = None
-        self._remain = None
-        self._remain_len = 0
+        self._tree = None           #searchable suffix tree
+        self._index_dict = None     #dictionary from raw_index to volume, issue, article and index
+        self._ngram = None          #ngram object for entire search range
+        self._remain = None         #current search result
         self._load(DIRECTORY_NAME)
 
+
     def _load(self, dic_name, debug=False):
+        """reads the .txt files from the given directory and builds the ngram
+        and suffix tree data structures necessary for the search, as well as the
+        conversion dictionary"""
         total = ""
         tindex = 0
         total_dict = {}
         n = set()
-        v, i, a, ind = (0, 0, 0, 0)
+        v, i, a = (0, 0, 0)
         r = re.compile("(\d\d)(\d\d)(\d\d\d).txt")
         for filename in sorted(os.listdir(dic_name)):
             index = 0
@@ -92,8 +95,8 @@ class SM_Search:
                 v, i, a = map(int, r.match(filename).groups())
                 w = ''
                 with open(os.path.join(dic_name, filename), "rb") as file:
-                    for c in file.read().decode("utf8", errors="ignore").lower():
-                        if c in [' ', '\t', '\n', '\r']:
+                    for c in file.read().decode("utf8", errors="replace").lower():
+                        if c in [' ', '\t', '\n', '\r', '\ufffd']:
                             if total[-1] == ' ':
                                 index += 1
                                 continue
@@ -104,7 +107,7 @@ class SM_Search:
                         else:
                             total += c
                             w += c
-                        total_dict[tindex] = (v, i, a, ind)
+                        total_dict[tindex] = (v, i, a, index)
                         tindex += 1
                         index += 1
 
@@ -114,9 +117,11 @@ class SM_Search:
         self._ngram = NGram(n)
 
     def page_num(self):
-        return self._remain_len // PAGE_LIMIT + 1
+        """returns the number of pages of results currently stored in _remain"""
+        return (l := len(self._remain)) // PAGE_LIMIT + bool(l % PAGE_LIMIT)
 
     def _simplify_results(self, results):
+        """simplifies the results of a single fuzzy search"""
         results.sort(key=Result.getRawIndex)
         simplified = [results[0]]
         for i in range(len(results)):
@@ -131,36 +136,27 @@ class SM_Search:
         simplified.sort(key=Result.getThresh)
         return simplified
 
-    def _generate_results(self):
-        if self._remain:
-            r = [[r.id(), r.getPreview()] for r in self._remain[:PAGE_LIMIT]]
-            self._remain = self._remain[PAGE_LIMIT:]
-            return r
+    def generate_results(self, pg_num = 0):
+        """generate and return a page worth of results"""
+        if self._remain and pg_num <= self.page_num():
+            return [[r.id(), r.getPreview()] for r in self._remain[pg_num*PAGE_LIMIT: (pg_num + 1)*PAGE_LIMIT]]
         return []
 
-    def _set_results(self, string, ids=[], thresh=0.5):
-        results = []
-        for w in self._ngram.search(string, threshold=thresh):
-            results.extend([Result(*w, e, self._index_dict) for e in self._tree.find_all(w[0])])
-        if results:
-            results = self._simplify_results(results)
-            self._remain = filter((lambda r : r.id() in ids), results) if ids else results
-
-#pure literal search broken
-#occasional failure to properly retrieve preview
-#possible failure to properly find intersections for fuzzy search by word
-#investigate higher thresholding to reduce run time
-
     def search(self, string, ids=[], thresh=0.5):
+        """conduct a fuzzy search and/or literal search (denoted by a quoted
+        string) constrained to the given list of ids (if applicable) and returns
+        the first page of results. If no search term is passed, returns the
+        first page of results of the stored search"""
         string = string.strip().lower()
+        c = re.compile("\".*?\"")
         if string:
             ids = set(ids)
             idict = dict(zip(sorted(list(ids)), [-1]*len(ids)))
             #literal search for quoted search terms
-            for quote in (exact := re.findall("\".*\"", string)):
+            for quote in (exact := [s[1:-1] for s in c.findall(string)]):
                 s = set()
-                print(quote[1:-1])
-                for raw in self._tree.find_all(quote[1:-1]):
+                # print(quote)
+                for raw in self._tree.find_all(quote):
                     s.add(id := int("{:02d}{:02d}{:03d}".format(*(self._index_dict[raw][:3]))))
                     idict[id] = min([raw, idict.get(id, float('inf'))])
                 if ids:
@@ -169,13 +165,13 @@ class SM_Search:
                     ids = s
 
             #list of fuzzy search terms
-            if not (words := re.sub("\".*\"", "", string)):
+            if not (words := c.sub("", string).strip()):
                 # print("pure literal case")
                 # print(ids)
                 self._remain = [Result(exact, 1, idict[i], self._index_dict) for i in ids]
-                self._remain_len = len(self._remain)
                 # print(self._remain)
                 return self._generate_results()#results for pure literal search
+            # print(words)
             word = (words := words.split())[0]
             results = []
             for w in self._ngram.search(word, threshold=thresh):
@@ -184,10 +180,10 @@ class SM_Search:
                 return []
             results = self._simplify_results(results)
             self._remain = list(filter((lambda r : r.id() in ids), results)) if ids else results
-            self._remain_len = len(self._remain)
             rdict = dict(zip(list(map(Result.id, self._remain)), self._remain))
-            # print(rdict)
+            print(rdict)
             for word in words[1:]:
+                print("additional fuzzy")
                 if not self._remain:
                     return []
                 for w in self._ngram.search(word, threshold=thresh):
@@ -197,17 +193,17 @@ class SM_Search:
                         if (e := rdict.get(id)):
                             d[id] = e
                             e.terms += [w[0]]
-                            e.index = max([e.index, ind])
+                            e.index = min([e.index, ind])
                             e.thresh *= w[1]
                 rdict = d
             if rdict:
                 self._remain = list(rdict.values())
             self._remain.sort(key=Result.getThresh)
-            self._remain_len = len(self._remain)
 
         return self._generate_results()
 
 def main():
+    """testing command line interface"""
     tic = time.perf_counter()
     s = SM_Search()
     print(time.perf_counter() - tic)
