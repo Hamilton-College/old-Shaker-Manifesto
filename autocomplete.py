ed3286b78cab6cbfee00135a68e582735f871a96
nwadair <35549523+nwadair@users.noreply.github.com>
2020-11-19 20:47:03 -0500

    updated autcomplete to have both general and author autocomplete and wrapped inside object

--- autocomplete.py
+++ autocomplete.py
@@ -1,31 +1,29 @@
 from fast_autocomplete import AutoComplete
 
-AUTOCOMPLETE = None
+class SM_Autocomplete:
 
-def init():
-    names = None
-    with open("shaker_dictionary.txt", encoding="utf8") as file:
-        names = file.readlines()
+    def __init__(self):
+        with open("shaker_dictionary.txt", "r", encoding="utf8") as file:
+            words = file.readlines()
+        words = dict(zip(words, [dict()] * len(words)))
+        self.autocomplete = AutoComplete(words=words)
 
-    words = dict(zip(names, [dict()] * len(names)))
-    return AutoComplete(words=words)
+        with open("authors.txt", "r", encoding="utf8") as f:
+            names = file.readlines()
+        names = dict(zip(names, [dict()] * len(names)))
+        self.authors = AutoComplete(words=names)
 
 
-def search(s):
-    print("in autocomplete.py")
-    global AUTOCOMPLETE
-    if AUTOCOMPLETE is None:
-        AUTOCOMPLETE = init()
-    return sorted(AUTOCOMPLETE.search(word=s, max_cost=3, size=10))
+    def general(self, s):
+        return sorted(AUTOCOMPLETE.search(word=s, max_cost=3, size=10))
+
+    def author(self, s):
+        return sorted(AUTHORS.search(word=s, max_cost=3, size=10))
 
 def main():
-    AUTOCOMPLETE = init()
-    s = input("::>")
-    while s != "exit":
-        print(*search(s), sep='\n')
-        s = input("::>")
-
-
+    ac = SM_Autocomplete
+    while (s := input("::>")) != "exit":
+        print(*ac.general(s), sep='\n')
 
 
 if __name__ == "__main__":
--- gen_dictionary.py
+++ gen_dictionary.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 from functools import reduce
 
 SAVE_LOC = "shaker_dictionary.txt"
+COMMAND_LOC = "authors.txt"
 
 def get_xml_names(filename):
     tree = ET.parse(filename)
@@ -17,6 +18,18 @@ def xml_traverse(root, names):
             names.append(elem.attrib.get("reg"))
         xml_traverse(elem, names)
 
+def create_names():
+    with open("names.csv", "r") as f:
+        contents = f.read()
+    authors = set()
+    for n in contents.split('\n'):
+        authors.add(re.sub("[\"\']", "", n))
+    authors.discard("")
+    authors.discard("regauthor")
+    with open(COMMAND_LOC, "w+", encoding="utf8") as output:
+        output.write("\n".join(sorted(list(authors))))
+    print(sorted(list(authors)))
+
 def create_dictionary(directory):
     s = set()
     for filename in os.listdir(directory):
@@ -26,12 +39,6 @@ def create_dictionary(directory):
                 file = f.read().decode("utf8", errors="ignore")
                 for w in file.split():
                     s.add(re.sub("[-.,\"?\\\\/()!#$;:%&{}=0-9'*+|~\[\]^_]", "", w).lower())
-            # with open(os.path.join(directory, filename), encoding="utf8") as f:
-            #     file = f.read()
-            #     for w in file.split():
-            #         if w.isascii():
-            #             w = re.sub("[-.,\"?\\\\/()!#$;:%&{}=0-9'*+|~\[\]^_]", "", w)
-            #             s.add(w.lower())
 
     with open("general_dictionary.txt", "r") as f:
         l = f.read().split()
@@ -50,7 +57,10 @@ def create_dictionary(directory):
 
 def main():
     if len(sys.argv) == 2:
-        create_dictionary(sys.argv[1])
+        if sys.argv[1] == '-a':
+            create_names()
+        else:
+            create_dictionary(sys.argv[1])
     elif len(sys.argv) == 3 and sys.argv[1] == '-n':
         n = get_xml_names(sys.argv[2])
         print(n)
@@ -73,7 +83,6 @@ def main():
                 l.append(" ".join(ns.split()))
         l.remove("Editor")
         print(*[str(n).strip() for n in l], sep='\n')
-
     else:
         print("Command Line args are invalid.")
 
