ffcac5714cd01ae0bb8cec8305c3220102a91f8e
nwadair <35549523+nwadair@users.noreply.github.com>
2020-11-19 21:07:35 -0500

    patches to autocomplete

--- autocomplete.py
+++ autocomplete.py
@@ -3,27 +3,27 @@ from fast_autocomplete import AutoComplete
 class SM_Autocomplete:
 
     def __init__(self):
-        with open("shaker_dictionary.txt", "r", encoding="utf8") as file:
-            words = file.readlines()
+        with open("shaker_dictionary.txt", "r", encoding="utf8") as f:
+            words = f.readlines()
         words = dict(zip(words, [dict()] * len(words)))
         self.autocomplete = AutoComplete(words=words)
 
         with open("authors.txt", "r", encoding="utf8") as f:
-            names = file.readlines()
+            names = f.readlines()
         names = dict(zip(names, [dict()] * len(names)))
         self.authors = AutoComplete(words=names)
 
-
     def general(self, s):
-        return sorted(AUTOCOMPLETE.search(word=s, max_cost=3, size=10))
+        return sorted(self.autocomplete.search(word=s, max_cost=3, size=10))
 
     def author(self, s):
-        return sorted(AUTHORS.search(word=s, max_cost=3, size=10))
+        return sorted(self.authors.search(word=s, max_cost=3, size=10))
 
 def main():
-    ac = SM_Autocomplete
+    ac = SM_Autocomplete()
     while (s := input("::>")) != "exit":
-        print(*ac.general(s), sep='\n')
+        print(s)
+        print(*(ac.general(s)), sep='\n')
 
 
 if __name__ == "__main__":
